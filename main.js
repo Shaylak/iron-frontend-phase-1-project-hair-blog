657ae73b65b9b28bd4cc88052b6b1aae118bd984
Shaylak <shayyylaaaking@gmail.com>
2023-07-23 19:04:52 -0700

    update to js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('connected')
+const hairPatterns = {
+    pattern1: "Kinky Curly",
+    pattern2: "Jerry Curl",
+    pattern3: "Straight",
+    pattern4: "Body Wave",
+    pattern5: "Water Wave"
+};
+
+hairPatterns()
