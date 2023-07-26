4d4c9796e0779bd18710db5510bd21877dafbd27
Shaylak <shayyylaaaking@gmail.com>
2023-07-25 21:51:44 -0700

    API

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 
 
-console.log('main.js connected')
-
-const handleFormInputFocus = () => {
-    console.log(`focus occurred`)
-
-}
-
-
 document.getElementById("myForm").addEventListener("submit", myFunction);
 
 function myFunction() {
@@ -23,3 +15,38 @@ document.getElementById('button').addEventListener('click', myFunction2);
 function myFunction2() {
     alert("Post Deleted")
 };
+
+
+
+
+function fetchVideos() {
+    fetch("url")
+      .then((response) => response.API_KEY())
+      .then((renderVideo) => console.log(fetchVideos));
+  
+     return renderVideos;
+    };
+
+import YTSearch from 'youtube-api-searc';
+
+const API_KEY = Process.env.REACT_APP_API_KEY_YT
+
+videoSearch = (term) => {
+    YTSearch( {key: API_KEY, term:term}, (videos) => {
+
+console.log(videos)
+    })
+}
+
+/*const player;
+function onYouTubeIframeAPIReady() {
+player = new YT.Player('player', {
+height: '240',
+width: '640',
+videoId: 'M7lc1UVf-VE',
+events: {
+'onReady': onPlayerReady,
+'onStateChange': onPlayerStateChange
+}
+});
+}
